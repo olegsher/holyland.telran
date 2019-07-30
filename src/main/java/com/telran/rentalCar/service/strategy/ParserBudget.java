8a6ece4726a921e2e984cc5022f212a22d0566ef
RuslanMuha <fizmategpu@gmail.com>
2019-07-30 10:33:48 +0300

    refactored names classes

--- src/main/java/com/telran/rentalCar/service/strategy/ParserBudget.java
+++ src/main/java/com/telran/rentalCar/service/strategy/ParserBudget.java
@@ -2,11 +2,9 @@ package com.telran.rentalCar.service.strategy;
 
 import com.telran.rentalCar.service.Parser;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ParserBudget implements Parser {
-    @Value("${parser.url.albar}")
+    @Value("${parser.url.budget}")
     String url;
 
     @Override
