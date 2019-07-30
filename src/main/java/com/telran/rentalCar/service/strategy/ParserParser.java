bcfb9620b21f9897f3f46a490e1bc35392501a33
Natalya <smilyk1982@gmail.com>
2019-07-30 10:08:31 +0300

    test

--- src/main/java/com/telran/rentalCar/service/strategy/ParserParser.java
+++ src/main/java/com/telran/rentalCar/service/strategy/ParserParser.java
@@ -4,6 +4,7 @@ import com.telran.rentalCar.service.Parser;
 import org.springframework.beans.factory.annotation.Value;
 
 public class ParserParser implements Parser {
+//    this is a parser for Albar
     @Value("${parser.url.budget}")
     String url;
 
