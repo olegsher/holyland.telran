2110d73d0a553618d502d3fca2260743ddaa6d4b
RuslanMuha <fizmategpu@gmail.com>
2019-08-20 15:53:42 +0300

    f

--- src/main/java/com/telran/rentalCar/controller/RentalController.java
+++ src/main/java/com/telran/rentalCar/controller/RentalController.java
@@ -1,8 +1,14 @@
 package com.telran.rentalCar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RentalController {
     //get/post data of rental
+
+    @GetMapping("/")
+    public String example() {
+        return "Hello";
+    }
 }
